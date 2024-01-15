a14e111d83f0767153f60913c55f576d3bb4a734
barnii77 <david.harrer@gmx.at>
2024-01-15 22:14:46 +0100

    kept working

--- src/lexer.rs
+++ src/lexer.rs
@@ -55,6 +55,12 @@ pub(crate) enum Token {
     Comma,
 }
 
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 impl FromStr for Token {
     type Err = JsonLexError;
 
--- src/parser.rs
+++ src/parser.rs
@@ -5,6 +5,11 @@ use thiserror::Error;
 // TODO create a datastructure that can store the json
 // TODO create a parser that can parse the tokenized json to this datastructure
 
+pub enum JsonError {
+    JsonLexError(JsonLexError),
+    JsonParseError(JsonParseError),
+}
+
 #[derive(Debug, Error)]
 pub enum JsonParseError {
     #[error("JsonParseError: Invalid token: {0}")]
@@ -29,9 +34,14 @@ enum DictParseState {
     ExpectCommaOrEnd,
 }
 
+enum ListParseState {
+    ExpectValue,
+    ExpectCommaOrEnd,
+}
+
 fn parse_dict(tokens: &[Token]) -> Result<Json, JsonParseError> {
     let mut state = DictParseState::ExpectKey;
-    let mut result_hashmap = Hashmap::new();
+    let mut result_hashmap = HashMap::new();
     let mut prev_key = String::new();
     let mut tokens_iter = tokens.iter();
     while let Some(token) = tokens_iter.next() {
@@ -51,25 +61,53 @@ fn parse_dict(tokens: &[Token]) -> Result<Json, JsonParseError> {
                 }
                 _ => return Err(JsonParseError::UnexpectedToken(token.clone())),
             },
-            DictParseState::ExpectValue => match token {
-                Token::Constant(c) => {
-                    result_hashmap.insert(prev_key, Json::Value(c.clone()));
-                    state = DictParseState::ExpectCommaOrEnd;
-                }
-                Token::StartOfDict => {
-                    // find end of dict
-                    let mut depth = 1;
-                    let mut context_tokens = Vec::new();
-                    while let Some(token) = tokens_iter.next() {
-                        context_tokens.push(token.clone()); // TODO avoid cloning by taking
-                        // ownership of tokens (this function won't need them anymore afterwards)
+            DictParseState::ExpectValue => {
+                match token {
+                    Token::Constant(c) => {
+                        result_hashmap.insert(prev_key, Json::Value(c.clone()));
                     }
-                },
-                Token::StartOfList => {
-                    todo!()
-                },
-                _ => { return Err(JsonParseError::UnexpectedToken(token.clone())) },
-            },
+                    Token::StartOfDict => {
+                        // find end of dict
+                        let mut depth = 1;
+                        let mut context_tokens = Vec::new();
+                        while let Some(token) = tokens_iter.next() {
+                            match token {
+                                Token::StartOfDict => depth += 1,
+                                Token::EndOfDict => depth -= 1,
+                                _ => {}
+                            }
+                            if depth <= 0 {
+                                break;
+                            }
+                            context_tokens.push(token.clone()); // TODO avoid cloning by taking
+                                                                // ownership of tokens (this function won't need them anymore afterwards)
+                        }
+                        let sub_json = parse_dict(&context_tokens)?;
+                        result_hashmap.insert(prev_key, sub_json);
+                    }
+                    Token::StartOfList => {
+                        // find end of dict
+                        let mut depth = 1;
+                        let mut context_tokens = Vec::new();
+                        while let Some(token) = tokens_iter.next() {
+                            match token {
+                                Token::StartOfList => depth += 1,
+                                Token::EndOfList => depth -= 1,
+                                _ => {}
+                            }
+                            if depth <= 0 {
+                                break;
+                            }
+                            context_tokens.push(token.clone()); // TODO avoid cloning by taking
+                                                                // ownership of tokens (this function won't need them anymore afterwards)
+                        }
+                        let sub_json = parse_list(&context_tokens)?;
+                        result_hashmap.insert(prev_key, sub_json);
+                    }
+                    _ => return Err(JsonParseError::UnexpectedToken(token.clone())),
+                }
+                state = DictParseState::ExpectCommaOrEnd;
+            }
             DictParseState::ExpectCommaOrEnd => match token {
                 Token::Comma => {
                     state = DictParseState::ExpectKey;
@@ -79,9 +117,113 @@ fn parse_dict(tokens: &[Token]) -> Result<Json, JsonParseError> {
             },
         }
     }
-    Ok(Json::Value(Constant::Null)) // FIXME so the lsp will leave me alone
+    Ok(Json::Dict(result_hashmap))
 }
 
-fn parse_list(tokens: &[Token]) -> Json {}
+fn parse_list(tokens: &[Token]) -> Result<Json, JsonParseError> {
+    let mut state = ListParseState::ExpectValue;
+    let mut result_vec: Vec<Json> = Vec::new();
+    let mut tokens_iter = tokens.iter();
+    while let Some(token) = tokens_iter.next() {
+        match state {
+            ListParseState::ExpectValue => {
+                match token {
+                    Token::Constant(c) => result_vec.push(Json::Value(c.clone())),
+                    Token::StartOfDict => {
+                        // find end of dict
+                        let mut depth = 1;
+                        let mut context_tokens = Vec::new();
+                        while let Some(token) = tokens_iter.next() {
+                            match token {
+                                Token::StartOfDict => depth += 1,
+                                Token::EndOfDict => depth -= 1,
+                                _ => {}
+                            }
+                            if depth <= 0 {
+                                break;
+                            }
+                            context_tokens.push(token.clone()); // TODO avoid cloning by taking
+                                                                // ownership of tokens (this function won't need them anymore afterwards)
+                        }
+                        let sub_json = parse_dict(&context_tokens)?;
+                        result_vec.push(sub_json);
+                    }
+                    Token::StartOfList => {
+                        // find end of dict
+                        let mut depth = 1;
+                        let mut context_tokens = Vec::new();
+                        while let Some(token) = tokens_iter.next() {
+                            match token {
+                                Token::StartOfList => depth += 1,
+                                Token::EndOfList => depth -= 1,
+                                _ => {}
+                            }
+                            if depth <= 0 {
+                                break;
+                            }
+                            context_tokens.push(token.clone()); // TODO avoid cloning by taking
+                                                                // ownership of tokens (this function won't need them anymore afterwards)
+                        }
+                        let sub_json = parse_list(&context_tokens)?;
+                        result_vec.push(sub_json);
+                    }
+                    _ => return Err(JsonParseError::UnexpectedToken(token.clone())),
+                }
+                state = ListParseState::ExpectCommaOrEnd;
+            }
+            ListParseState::ExpectCommaOrEnd => match token {
+                Token::Comma => state = ListParseState::ExpectValue,
+                _ => return Err(JsonParseError::UnexpectedToken(token.clone())),
+            },
+        }
+    }
+    Ok(Json::List(result_vec))
+}
 
-pub fn parse(json: &str) -> Json {}
+pub fn parse(json: &str) -> Result<Json, JsonError> {
+    let tokens_result = tokenize(json);
+    let tokens: Vec<Token> = match tokens_result {
+        Ok(tokens) => tokens,
+        Err(e) => return Err(JsonError::JsonLexError(e)),
+    };
+    if tokens.len() < 2 {
+        // If there is only one token, there cannot even be a pair of opening and
+        // closing brackets. Therefor, must be invalid. This check is mostly there for avoiding
+        // future leg-shooting if I change something
+        return Err(JsonError::JsonParseError(
+            JsonParseError::UnexpectedEndOfInput,
+        ));
+    }
+    let first_token_option = tokens.iter().next();
+    let first_token = match first_token_option {
+        Some(token) => token,
+        None => {
+            return Err(JsonError::JsonParseError(
+                JsonParseError::UnexpectedEndOfInput,
+            ))
+        }
+    };
+    let last_token_option = tokens.iter().last();
+    let last_token = match last_token_option {
+        Some(token) => token,
+        None => {
+            return Err(JsonError::JsonParseError(
+                JsonParseError::UnexpectedEndOfInput,
+            ))
+        }
+    };
+    if first_token != last_token {
+        return Err(JsonError::JsonParseError(JsonParseError::UnexpectedToken(
+            last_token,
+        )));
+    }
+    let remaining_tokens = &tokens[1..tokens.len() - 1];
+    match first_token {
+        Token::StartOfDict => parse_dict(remaining_tokens), // TODO if error, wrap in
+        // JsonError::JsonParseError
+        Tokens::StartOfList => parse_list(remaining_tokens),
+        _ => Err(JsonError::JsonParseError(
+            JsonParseError::UnexpectedEndOfInput,
+        )),
+    }
+}
